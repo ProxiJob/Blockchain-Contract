128d83fb9a948c32281b4db2d86a6711c4192076
Etienne PASSOT <etienne.passot@epitech.eu>
2018-04-09 11:42:44 +0200

    [+] add contract + encode/decode

--- addGetContract.js
+++ addGetContract.js
@@ -13,7 +13,27 @@ const bitswap = require('ipfs-api/src/bitswap')('/ip4/127.0.0.1/tcp/5001')
 /***** Init Object Bdd *****/
 var Contract = Parse.Object.extend("Contracts");
 var contract = new Contract();
+var base64 = require('file-base64');
 
+function pdfTo64File(filename) {
+	base64.encode(filename, function(err, base64String) {
+		var fs = require('fs');
+
+		fs.appendFile('b64.txt', base64String, function (err) {
+      id = addContractInIPFS("b64.txt", "contractmail");
+		  if (err) throw err;
+      console.log('Saved!');
+      console.log(id);
+		});
+	});
+}
+
+function fileContentB64ToPdf(filename, content) {
+	var fs = require('fs');
+	base64.decode(content, filename, function(err, output) {
+		console.log('success');
+	});
+}
 
 function addContractInIPFS(file, name) {
   ipfs.util.addFromFs(file, {}, (err, result) => {
@@ -41,7 +61,6 @@ function getContractInDB(name) {
   query.equalTo("name", name);
   query.find({
     success: function(contract) {
-      console.log(contract[0].get('contract'));
       getContractInIPFS(contract[0].get('contract'));
       },
     error: function(object, error) {
@@ -54,14 +73,11 @@ function getContractInIPFS(hash) {
   ipfs.files.cat('/ipfs/' + hash, function (err, file) {
     if (err) {
       throw err
-    }  
-    console.log(file.toString('utf8'))
+    }
+    fileContentB64ToPdf("new.pdf", file.toString('utf8'));
   })  
 }
 
-//id = addContractInIPFS("tmp/toto.txt", "contractmail");
+//pdfTo64File("tmp/mail.pdf");
 
-//getContractInDB("QmRbf6WxUAsin1PKPbhuQ9uTKA2LybqAAe4UJB2yndf8BL");
-
-
-getContractInIPFS("QmRbf6WxUAsin1PKPbhuQ9uTKA2LybqAAe4UJB2yndf8BL");
+getContractInIPFS("QmdFNYXxcFTSfqPyjLVRRPeR1q8zsHhqfiYcXJzYpzhxNr");
